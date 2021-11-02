88c955ee509737c6660b01222671a433df0c63c4
kaszub09 <kaszub09@gmail.com>
2021-11-02 23:33:14 +0100

    Release doesn't run beccause of assemblyName  change.

--- WPFSampleApplication/Properties/AssemblyInfo.cs
+++ WPFSampleApplication/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("WPFSampleApplication")]
+[assembly: AssemblyTitle("Notepad++++")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPFSampleApplication")]
+[assembly: AssemblyProduct("Notepad++++")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
